6444c33961ddd969753ade11f9761faa446cb595
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-29 21:09:19 +0100

    Added toolbar

--- Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/MainActivity.java
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/MainActivity.java
@@ -3,6 +3,10 @@ package tech.szymanskazdrzalik.self_diagnosis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_profile:
+                Toast.makeText(this, "New profile", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.choose_profile:
+                Toast.makeText(this, "Choose profile", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
