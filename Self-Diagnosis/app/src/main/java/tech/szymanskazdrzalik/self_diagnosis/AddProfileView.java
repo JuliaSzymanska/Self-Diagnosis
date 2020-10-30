5bdaa62dc533bf9ddd1cc7de040060938405e804
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-30 15:43:18 +0100

    Added new biew

---
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/AddProfileView.java
@@ -0,0 +1,11 @@
+package tech.szymanskazdrzalik.self_diagnosis;
+
+import android.content.Context;
+import android.view.View;
+
+public class AddProfileView extends View {
+
+    public AddProfileView(Context context) {
+        super(context);
+    }
+}
---
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/FirstActivity.java
@@ -0,0 +1,14 @@
+package tech.szymanskazdrzalik.self_diagnosis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
