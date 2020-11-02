559266ff6ea67980c083691920f78ab9b34b3721
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-11-02 13:13:45 +0100

    db skeleton

--- Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/db/SampleSQLiteDBHelper.java
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/db/SampleSQLiteDBHelper.java
@@ -9,25 +9,32 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class SampleSQLiteDBHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "user_profiles";
     public static final String USER_PROFILE_TABLE_NAME = "users";
     public static final String USER_COLUMN_ID = "user_id";
     public static final String USER_COLUMN_GENDER = "gender";
     public static final String USER_COLUMN_BIRTH_DATE = "birth_date";
+    public static final String USER_COLUMN_NAME = "user_name";
+    private static final int DATABASE_VERSION = 1;
+
     /**
      * {@inheritDoc}
      */
-   public SampleSQLiteDBHelper(Context context) {
-       super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public SampleSQLiteDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
 
-   }
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + USER_PROFILE_TABLE_NAME + " (" +
+                USER_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                USER_COLUMN_NAME + " TEXT," +
+                USER_COLUMN_BIRTH_DATE + " DATE," +
+                USER_COLUMN_GENDER + " TEXT " + ")");
 
     }
 
@@ -36,6 +43,7 @@ public class SampleSQLiteDBHelper extends SQLiteOpenHelper {
      */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + USER_PROFILE_TABLE_NAME);
+        onCreate(db);
     }
 }
