c59fcdfc6bb3971faddff2d39b5b5aa891e0518d
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-11-02 20:47:15 +0100

    todos

--- Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/AddProfileView.java
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/AddProfileView.java
@@ -4,6 +4,18 @@ import android.content.Context;
 import android.view.View;
 
 public class AddProfileView extends View {
+    // TODO: 02.11.2020 - koniecznie przed prezentacją
+    //  Menu po cofnięciu się w czacie z botem - Zmiana użytkownika, dodanie nowego użytkownika, historia leczenia użytkownika
+    //  Dodać pokazowe przyciski do odpowiedzi do bota (nie powiązane z api, na rzecz prezentacji)
+
+    // TODO: 02.11.2020 Mieszane odczucia co do kiedy
+    //  Pierwsze uruchomienie aplikacj - utworzenie uzytkownika ewentualnie pokaz możliwości aplikacji
+
+    // TODO: 02.11.2020 - raczej po prezentacji
+    //  Baza danych - dodać tabelę z czatami, powiązane z id użytkownika
+    //  Baza danych - zapisywać rozmowę - diagnoza, zapisujemy jednynie ukonczone diagnozy
+    //  Interakcja z api
+    //  Dodawanie zdj profilowego (dodać do bazy danych)
 
     public AddProfileView(Context context) {
         super(context);
