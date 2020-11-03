a0342cc1985238420383a9814fd0c98918d2cd4e
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-11-03 19:48:27 +0100

    Added fragment

--- Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/AddProfileView.java
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/AddProfileView.java
@@ -1,9 +1,20 @@
 package tech.szymanskazdrzalik.self_diagnosis;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Gallery;
 
-public class AddProfileView extends View {
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import static android.app.Activity.RESULT_OK;
+
+public class AddProfileView extends Fragment {
+    Button button;
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1000;
     // TODO: 02.11.2020 - koniecznie przed prezentacją
     //  Menu po cofnięciu się w czacie z botem - Zmiana użytkownika, dodanie nowego użytkownika, historia leczenia użytkownika
     //  Dodać pokazowe przyciski do odpowiedzi do bota (nie powiązane z api, na rzecz prezentacji)
@@ -17,7 +28,16 @@ public class AddProfileView extends View {
     //  Interakcja z api
     //  Dodawanie zdj profilowego (dodać do bazy danych)
 
-    public AddProfileView(Context context) {
-        super(context);
+    private void addProfileImageListener(View v) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            //button.background(setImageUri(data.getData()); czy cos takiego
+             }
     }
 }
