85442d5b78db234f352039ad348363456a14a7db
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-11-03 22:56:59 +0100

    Problem with fragment

--- Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/FirstActivity.java
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/FirstActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
     // TODO: 02.11.2020 https://developer.android.com/topic/libraries/view-binding#java
@@ -12,9 +13,19 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        Fragment fragment = new AddProfile();
-        ft.replace(R.id.fragmentAddProfile, fragment);
-        ft.commit();
+//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+//        ft.replace(R.id.fragmentAddProfile, fragment);
+//        ft.commit();
+
     }
+
+    public void onC(View v){
+        Fragment fragment = new AddProfile();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fg_add_profile, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+    }
+
 }
