f0545ff8b644d1587607f7f212be1252a05e3142
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-11-19 21:25:12 +0100

    RequestQueue

---
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/api/ApiRequestQueue.java
@@ -0,0 +1,15 @@
+package tech.szymanskazdrzalik.self_diagnosis.api;
+
+import android.content.Context;
+
+public class ApiRequestQueue {
+
+    private static ApiRequestQueue INSTANCE;
+    private RequestQueue requestQueue;
+
+    private ApiRequestQueue(Context context){
+
+
+    }
+
+}
