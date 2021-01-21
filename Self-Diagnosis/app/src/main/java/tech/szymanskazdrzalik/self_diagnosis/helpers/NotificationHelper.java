da0af56ab186246062e90ef6d833589f17afecc6
JuliaSzymanska <szymanska.julia.it@gmail.com>
2021-01-21 15:06:16 +0100

    Added icon to notification

--- Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/helpers/NotificationHelper.java
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/helpers/NotificationHelper.java
@@ -1,12 +1,8 @@
 package tech.szymanskazdrzalik.self_diagnosis.helpers;
 
-import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-
-import tech.szymanskazdrzalik.self_diagnosis.SplashActivity;
 
 import static android.content.Context.ALARM_SERVICE;
 
@@ -15,7 +11,7 @@ public class NotificationHelper {
     public static final int DAYS_NOTIFICATION_DELAY = 7;
 
     public static void setNotification(PendingIntent pendingIntent, Context context) {
-        AlarmManager am = (AlarmManager)context.getSystemService(ALARM_SERVICE);
+        AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
         am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_DAY * DAYS_NOTIFICATION_DELAY, pendingIntent);
 
     }
--- Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/helpers/Receiver.java
+++ Self-Diagnosis/app/src/main/java/tech/szymanskazdrzalik/self_diagnosis/helpers/Receiver.java
@@ -3,16 +3,13 @@ package tech.szymanskazdrzalik.self_diagnosis.helpers;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.job.JobScheduler;
-import android.app.job.JobService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
 
 import androidx.core.app.NotificationCompat;
 
-import tech.szymanskazdrzalik.self_diagnosis.Menu;
+import tech.szymanskazdrzalik.self_diagnosis.R;
 import tech.szymanskazdrzalik.self_diagnosis.SplashActivity;
 
 public class Receiver extends BroadcastReceiver {
@@ -25,7 +22,7 @@ public class Receiver extends BroadcastReceiver {
         Intent intent = new Intent(context, SplashActivity.class);
         PendingIntent pi = PendingIntent.getActivity(context, NotificationHelper.REQUEST_CODE, intent, 0);
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-//                .setSmallIcon(R.drawable.android_icon)
+                .setSmallIcon(R.drawable.doctor)
                 .setContentTitle("Title")
                 .setContentText("Some text");
         mBuilder.setContentIntent(pi);
